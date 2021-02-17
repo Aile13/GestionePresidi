c66499189c0bbde0db88367b91d257b3de2c2f45
Aile <600eli@gmail.com>
2021-02-17 16:54:56 +0100

    Initial commit

---
+++ src/main/java/it/eliapitozzi/gestionepresidi/GestionepresidiApplication.java
@@ -0,0 +1,13 @@
+package it.eliapitozzi.gestionepresidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionepresidiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionepresidiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/eliapitozzi/gestionepresidi/GestionepresidiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eliapitozzi.gestionepresidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionepresidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
